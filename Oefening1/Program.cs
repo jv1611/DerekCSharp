1a6bdfbac7ecd1ddcd721c5857bd6c2c6fbb5e26
Jochen J.J.J <jochenvervoort@gmail.com>
2018-11-26 14:53:04 +0100

    for loop, commandline Args en name function

--- Oefening1/Program.cs
+++ Oefening1/Program.cs
@@ -7,7 +7,26 @@ namespace Oefening1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            for(int i=0; i<args.Length; i++)
+            {
+                Console.WriteLine("Argument {0} : {1}", i, args[i]);
+            }
+
+            string[] myArgum = Environment.GetCommandLineArgs();
+            Console.WriteLine(string.Join(", ", myArgum));
+
+            SayHello();
+
             Console.ReadKey();
+        }
+
+        private static void SayHello()
+        {
+            string name = "";
+            Console.Write("What's your name? : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
         }
     }
 }
