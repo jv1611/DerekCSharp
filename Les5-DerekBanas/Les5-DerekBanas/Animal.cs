472c0ed83c1afc268ab0fea46d985682e7d9b771
Jochen J.J.J <jochenvervoort@gmail.com>
2019-03-06 12:04:39 +0100

    Les 5 van Derek met Classes in aparte files

---
+++ Les5-DerekBanas/Les5-DerekBanas/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Les5_DerekBanas
+{
+    class Animal
+    {
+        public string name;
+        public string sound;
+
+        public Animal()
+        {
+            name = "No Name"; 
+            sound = "No Sound";
+            numOfAnimals++;
+        }
+
+        public Animal(string name = "No Name")
+        { // gegevens zijn hier niet echt nodig er is al een default hierboven
+            this.name = name;
+            this.sound = "No Sound";
+            numOfAnimals++;
+        }
+
+        public Animal(string name= "No Name", string sound = "No Sound")
+        { // gegevens zijn hier niet echt nodig er is al een default hierboven
+            this.sound = sound;
+            this.name = name;
+            numOfAnimals++;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+
+        static int numOfAnimals = 0;
+
+        public static int GetNumAnimals()
+        {
+            return numOfAnimals;
+        }
+
+    }
+}
---
+++ Les5-DerekBanas/Les5-DerekBanas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Les5_DerekBanas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rectangle rect1;
+            //rect1.length = 200;
+            //rect1.width = 50;
+            //Console.WriteLine("Area of rect1: {0}", rect1.Area());
+
+            //Rectangle rect2 = new Rectangle(110, 40);
+            //Console.WriteLine("Area of rectX: {0}", rect2.Area());
+
+            // Classes
+            // We hebben zojuist via Add de Class Animal.cs gemaakt
+            //Animal fox = new Animal()
+            //{
+            //    name = "Red",
+            //    sound = "Raaww"
+            //};
+
+            //fox.MakeSound();
+
+            //Animal koe = new Animal("Berta", "Melken");
+
+            //koe.MakeSound();
+
+            //Console.WriteLine("# of Animals : {0}",
+            //    Animal.GetNumAnimals());
+
+            // Nu hebben we ShapeMath.cs gemaakt
+            Console.WriteLine("Area of Rectangle: {0}", ShapeMath.GetArea("rectangle", 5, 6));
+            // Rectangle in .GetArea moet hetzelfde geschreven zijn als in ShapeMath.cs!!!
+            
+            Console.ReadLine();
+        }
+
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            public Rectangle(double l = 1, double w = 1)
+            {
+                length = l;
+                width = w;
+            }
+
+            public double Area() // = method
+            {
+                return length * width;
+            }
+
+        }
+    }
+}
